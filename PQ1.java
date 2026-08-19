61fe23e4334bbcdb5c7a5309d4e5f44b28072b88
sandeep kumar <122988674+sandeep07-k@users.noreply.github.com>
2026-08-19 19:05:24 +0530

    Update PQ1.java

--- PQ1.java
+++ PQ1.java
@@ -18,24 +18,4 @@ public class PQ1 {
 
     }
 }
-=======
-// Write a program to print whether a number is even or odd, also take input from the user.
-import java.util.Scanner;
 
-public class PQ1 {
-    
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        System.out.print("Enter the number: ");
-        int number = input.nextInt();
-
-        if(number%2==0){
-            System.out.println("The number "+ number + " is Even");
-        }else{
-            System.out.println("The number "+ number + " is Odd");
-        }
-
-    }
-}
->>>>>>> bec9f1f3a66a6ba01590ab2aa872b2c79fb1ea12
