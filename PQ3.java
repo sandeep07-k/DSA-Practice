597e536e9043bccc1ce98387c2325f9532004ea1
Sandeep <sandipkumar9334@gamil.com>
2026-08-20 22:05:06 +0530

    PQ3

--- PQ3.java
+++ PQ3.java
@@ -1,7 +1,26 @@
-// To calculate Fibonacci Series up to n numbers.
-
+// To calculate Fibonacci Series up to n numbers. 0,1,1,2,3,5,8,13,21......
+import java.util.Scanner;
 public class PQ3 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int n = input.nextInt();
+
+        int prev=0;
+        int next=1;
+        System.out.print("Fibonacci Series Upto "+ n+": 0,1,");
+        for(int i=1;i<=n-2;i++){
+            
+            int sum = prev+next;
+            System.out.print(sum+",");
+            prev= next;
+            next=sum;
+            
+        }
+        System.out.print("....");
         
+        
+
+
     }
 }
